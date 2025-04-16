195d3753a22bebe7cc26e694f207b3bb136a6599
gyuri00 <skarbfl0802@gmail.com>
2025-04-16 16:24:03 +0900

    init

---
+++ src/test/java/com/appcenter/study/ticketing/TicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appcenter.study.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
